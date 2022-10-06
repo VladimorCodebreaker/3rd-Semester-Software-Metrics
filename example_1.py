7a5431a3b6076eb216db92ff8c0516cbead4360f
Vladimir Schmadlak <91912841+VladimorCodebreaker@users.noreply.github.com>
2022-10-06 13:16:12 +0300

    First Commit // Test Pylint

---
+++ example_1.py
@@ -0,0 +1,14 @@
+"""
+Pylint
+"""
+
+class car:
+    def __init__(self, color):
+        self.color = color
+
+my_car = car('blue')
+
+def crash(car1, car2): #pylint: disable=unused-argument
+    car1.color = 'burnt'
+
+crash(car('red'), my_car)
