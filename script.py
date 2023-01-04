20a9b56f78662045c3cf57af1384a90ed56f36e6
Vladimir Schmadlak <91912841+VladimorCodebreaker@users.noreply.github.com>
2023-01-04 11:44:38 +0200

    Third Commit - Added 1 Test Case, Improved Code Quality

--- script.py
+++ script.py
@@ -1,7 +1,11 @@
+"""Implementation of the LDEXP Function - SW Metrics Exam - Part 2"""
 import math
 
 
 def load_exponent(first_arg, second_arg):
+    """Returns x * (2**i). This is the inverse of the function frexp()."""
     if not (isinstance(first_arg, (int, float)) and isinstance(second_arg, (int, float))):
-        raise TypeError(f"Result must be either [int] or [float]! {type(first_arg)} {type(second_arg)}")
+        raise TypeError(f"Result must be either int or float! {type(first_arg)} {type(second_arg)}")
+    if (isinstance(first_arg, bool) or isinstance(second_arg, bool)):
+        raise TypeError(f"Arguments cannot be of type 'bool' ! {type(first_arg)} {type(second_arg)}")
     return math.ldexp(first_arg, second_arg)
--- test.py
+++ test.py
@@ -1,18 +1,29 @@
+"""Implementation of the Test Cases for the function 'load_exponent' - SW Metrics Exam - Part 2"""
 import unittest
 from script import load_exponent
 
 
 class TestLoadExponentMethod(unittest.TestCase):
+    """Test 'load_exponent' method"""
+
     def test_incorrect_args(self):
+        """Test function with incorrect arguments"""
         with self.assertRaises(TypeError):
             load_exponent("5.1", "7")
 
     def test_without_args(self):
+        """Test function without arguments"""
         with self.assertRaises(TypeError):
             load_exponent()
 
     def test_check_result(self):
+        """Test the result of the function"""
         self.assertEqual(load_exponent(9, 3), 72)
+
+    def test_boolean_args(self):
+        """Test function with boolean arguments"""
+        with self.assertRaises(TypeError):
+            load_exponent(True, False)
 
 
 if __name__ == "__main__":
