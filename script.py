22844febde92d510e72936693d18ced5d955d0bb
Vladimir Schmadlak <91912841+VladimorCodebreaker@users.noreply.github.com>
2023-01-04 11:23:46 +0200

    Second Commit - Implemented Function

--- script.py
+++ script.py
@@ -2,4 +2,6 @@ import math
 
 
 def load_exponent(first_arg, second_arg):
-    pass
+    if not (isinstance(first_arg, (int, float)) and isinstance(second_arg, (int, float))):
+        raise TypeError(f"Result must be either [int] or [float]! {type(first_arg)} {type(second_arg)}")
+    return math.ldexp(first_arg, second_arg)
